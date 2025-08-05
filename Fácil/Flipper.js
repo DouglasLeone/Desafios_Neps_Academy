b21c84266bd4c5b4fefa0bd18a41dcd381fc1824
Douglas Leone <douglasleone09@gmail.com>
2025-08-05 20:01:33 -0300

    Adicionando novos codigos e adaptando codigos

--- Fácil/Flipper.js
+++ Fácil/Flipper.js
@@ -1,13 +1,11 @@
 // Desafio Fliper Neps Academy
 
-// Lendo a entrada do exercício
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf8");
 
 let A = parseInt(input.split(" ")[0])
 let B = parseInt(input.split(" ")[1])
 
-// Seu código vai aqui
 if (A === 0) {
     console.log("C");
 } else {
--- SuperFacil/Bondinho.js
+++ SuperFacil/Bondinho.js
@@ -1,13 +1,9 @@
 // Desafio Bondinho
-
-// Lendo a entrada do exercício
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf8");
 
 let A = parseInt(input.split("\n")[0])
 let M = parseInt(input.split("\n")[1])
-
-// Seu código vai aqui
 
 if (A + M > 50) {
     console.log("N")
